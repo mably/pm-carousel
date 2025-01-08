34967fd497a33ad29a2cdfa436950ed5af21e7c5
François <f.mably@gmail.com>
2025-01-08 15:11:32 +0100

    Swipe handling improvements

--- src/onTouch.js
+++ src/onTouch.js
@@ -1,4 +1,4 @@
-import { TRANSITION } from "./constants"
+import { SLIDE_MIN_RATIO, TRANSITION } from "./constants"
 
 const timeouts = {
 	onTouchStart: null,
@@ -20,7 +20,10 @@ export function onTouchStart(ev) {
 
 		this.nodes.overflow.style.transition = "none"
 		this._metrics.touchstartX = Math.round(ev.touches[0].pageX)
+		this._metrics.touchstartY = Math.round(ev.touches[0].pageY)
 		this._metrics.slideWidth = this.nodes.wrapper.offsetWidth
+		this._metrics.isScrolling = false
+		this._metrics.isSwiping = false
 	})
 }
 
@@ -28,26 +31,51 @@ export function onTouchMove(ev) {
 	handleRequestAnimationFrame("onTouchMove", () => {
 		this._metrics.moveX =
 			this._metrics.touchstartX - Math.round(ev.touches[0].pageX)
+		this._metrics.moveY =
+			this._metrics.touchstartY - Math.round(ev.touches[0].pageY)
 
-		this.nodes.overflow.style.transform = `translateX(${
-			-this._metrics.distance - this._metrics.moveX
-		}px)`
+		// Check if the gesture is primarily horizontal or vertical
+		if (!this._metrics.isScrolling && !this._metrics.isSwiping) {
+			if (Math.abs(this._metrics.moveX) > Math.abs(this._metrics.moveY)) {
+				this._metrics.isSwiping = true // Horizontal swipe
+			} else {
+				this._metrics.isScrolling = true // Vertical scroll
+			}
+		}
+
+		if (this._metrics.isSwiping) {
+			document.documentElement.style.overflow = "hidden"
+			this.nodes.overflow.style.transform = `translateX(${
+				-this._metrics.distance - this._metrics.moveX
+			}px)`
+		}
 	})
 }
 
 export function onTouchEnd() {
 	handleRequestAnimationFrame("onTouchEnd", () => {
-		const goToNext = this._metrics.moveX > this._metrics.slideWidth / 3
-		const goToPrev = this._metrics.moveX < -this._metrics.slideWidth / 3
+		if (!this._metrics.isSwiping) {
+			return
+		}
 
-		this.nodes.overflow.style.transition = TRANSITION
+		const goToNext =
+			this._metrics.moveX > this._metrics.slideWidth / SLIDE_MIN_RATIO
+		const goToPrev =
+			this._metrics.moveX < -this._metrics.slideWidth / SLIDE_MIN_RATIO
+
+		document.documentElement.style.removeProperty("overflow")
+
 		let newActive = this.activePage
 
 		// réinit moving distance
 		this._metrics.moveX = 0
 
+		this._metrics.isScrolling = false
+		this._metrics.isSwiping = false
+
 		if (!goToNext && !goToPrev) {
 			// reset to initial position
+			this.nodes.overflow.style.transition = TRANSITION
 			this.nodes.overflow.style.transform = `translateX(${-this._metrics
 				.distance}px)`
 			return
