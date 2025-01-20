d875d8fe594cb6e500ec82b97204989d731c1856
Funstenolf <sebastien.lefebvrepro@gmail.com>
2025-01-20 17:07:32 +0100

    feat(allow-incomplete-last-page): add a flag to display only the last elements even if the last page is incomplete (#5)
    Co-authored-by: Sébastien Lefebvre <sebastien.lefebvre@sully-group.fr>

--- src/updateScroll.js
+++ src/updateScroll.js
@@ -4,7 +4,7 @@ export function updateScroll() {
 	this._metrics.distance = this.activePage * this._metrics.slideWidth
 
 	// last slide
-	if (this.activePage === this.pagesLength - 1) {
+	if (!this.currentSettings.allowIncompleteLastPage && this.activePage === this.pagesLength - 1) {
 		this._metrics.distance =
 			this.nodes.overflow.scrollWidth - this._metrics.slideWidth
 
